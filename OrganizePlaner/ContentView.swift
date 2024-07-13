03af58661aeb4fb33a91fd67acd9e2c50e4fff03
Markus1986 <muellersmarkus@me.com>
2024-07-13 18:28:47 +0200

    Initial Commit

---
+++ OrganizePlaner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OrganizePlaner
+//
+//  Created by Markus Müller on 13.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OrganizePlaner/OrganizePlanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrganizePlanerApp.swift
+//  OrganizePlaner
+//
+//  Created by Markus Müller on 13.07.24.
+//
+
+import SwiftUI
+
+@main
+struct OrganizePlanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
